31bb90b98964fd7275e16ff5a093d82db3d139f6
江庆 <>
2019-12-25 19:40:29 +0800

    no message

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
 # vue-render
 
+有时候我们在写一个组件的时候，大体的组件架构是一样的，但是里面的内容有点不一样，比如有的数据多一条，有的只是字体不一样，有的多了一个图标，有的还有操作
+
+这样会让我们组件封装特别困难，写不全
+
+下面就用到 render 函数了，在 iviewUI 框架里面，列表 table 里面用到 render 函数，特别方便，想在里面自定义什么样的内容都可以
+
+我就想到我们自己写组件的时候，是不是也可以使用 render 来自定义内容呢
+
+这个小 demo 就用到 render 函数组件，在封装组件的时候，会更加方便
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<div v-for="(item,index) in list" :key="index">
+			<VueRender :index="index" :row="item.data" :render="item.render"></VueRender>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueRender from './components/vueRender'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		VueRender
+	},
+	data() {
+		return {
+			list: [
+				{
+					data: {
+						name: '科比',
+						age: 24
+					},
+					render: (h, params) => {
+						return h('div', [
+							h('span', params.row.name),
+							h('span', params.row.age)
+						])
+					}
+				},
+				{
+					data: {
+						name: '杜兰特',
+						age: 30
+					},
+					render: (h, params) => {
+						return h('div', [
+							h('span', { class: 'nameClass' }, params.row.name),
+							h('span', { style: { color: 'red' } }, params.row.age)
+						])
+					}
+				}
+			]
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 16px;
+}
+.nameClass {
+	color: blue;
 }
 </style>
